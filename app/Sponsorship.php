f4ce8ccdf3c5f3d55065678257d904b7671ac76f
Antonio Bove <antoniofbove@gmail.com>
2022-05-26 14:45:20 +0200

    sponsorships seeder

--- app/Sponsorship.php
+++ app/Sponsorship.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sponsorship extends Model
 {
+    protected $guarded = [];
+
     public function place() {
         return $this->belongsToMany('App\Place')
             ->withPivot('end_time');
--- database/seeds/SponsorshipSeeder.php
+++ database/seeds/SponsorshipSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Sponsorship;
 use Illuminate\Database\Seeder;
 
 class SponsorshipSeeder extends Seeder
@@ -11,6 +12,28 @@ class SponsorshipSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $sponsorships = [
+            [
+                'denomination' => 'basic',
+                'price' => 2.99,
+                'duration' => 24
+            ],
+            [
+                'denomination' => 'advanced',
+                'price' => 5.99,
+                'duration' => 72
+            ],
+            [
+                'denomination' => 'premium',
+                'price' => 9.99,
+                'duration' => 144
+            ],
+        ];
+
+        foreach ($sponsorships as $sponsorship) {
+            $new_sponsorship = new Sponsorship();
+            $new_sponsorship->fill($sponsorship);
+            $new_sponsorship->save();
+        }
     }
 }
