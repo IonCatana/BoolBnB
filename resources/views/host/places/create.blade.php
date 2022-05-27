410d39d1ae53e09dca803df09c325ce0a23455dd
Siripiri-hue <daemonprocellae@live.it>
2022-05-27 14:26:07 +0200

    Index table fixed

--- resources/views/host/places/create.blade.php
+++ resources/views/host/places/create.blade.php
@@ -3,26 +3,75 @@
 @section('content')
     <form action="{{ route('host.places.store') }}" method="POST">
         @csrf
-        <div>
-            <label for="title">Titolo</label>
-            <input type="text" class="form-control" id="title" name="title" placeholder="Titolo">
+
+        {{-- titolo --}}
+        <div class="form-group">
+            <label for="title">Denomination</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Enter a descriptive title">
         </div>
 
-        <div>
-            <label for="address">Indirizzo</label>
+        {{-- numero di stanze, letti, bagni e metri quadri --}}
+        <div class="form-group">
+            <label for="rooms">Number of rooms</label>
+            <input type="number" class="form-control" id="rooms" name="rooms" placeholder="Enter how many rooms the apartment has">
+        </div>
+
+        <div class="form-group">
+            <label for="beds">Number of beds</label>
+            <input type="number" class="form-control" id="beds" name="beds" placeholder="Enter how many beds the apartment has">
+        </div>
+
+        <div class="form-group">
+            <label for="bathrooms">Number of bathrooms</label>
+            <input type="number" class="form-control" id="bathrooms" name="bathrooms" placeholder="Enter how many bathrooms the apartment has">
+        </div>
+
+        <div class="form-group">
+            <label for="square_meters">Square meters</label>
+            <input type="number" class="form-control" id="square_meters" name="square_meters" placeholder="Enter how many square meters the apartment is">
+        </div>
+
+        {{-- indirizzo completo con lat e lng --}}
+        {{-- //TODO implementare tomtom --}}
+        <div class="form-group">
+            <label for="address">Address</label>
             <input type="text" class="form-control" id="address" name="address" placeholder="Indirizzo">
         </div>
 
-        <div>
-            <label for="lat">Latitudine</label>
+        <div class="form-group">
+            <label for="lat">Latitude</label>
             <input type="number" class="form-control" id="lat" name="lat" placeholder="Latitudine">
         </div>
 
-        <div>
-            <label for="lng">Longitudine</label>
+        <div class="form-group">
+            <label for="lng">Longitude</label>
             <input type="number" class="form-control" id="lng" name="lng" placeholder="Longitudine">
         </div>
 
-        <button type="submit">Aggiungi</button>
+        {{-- //TODO upload dell'immagine? --}}
+
+        {{-- servizi --}}
+        {{-- //TODO passare alla view lista di amenities --}}
+        <div class="form-group form-check form-check-inline">
+            {{-- foreach --}}
+            <input class="form-check-input" type="checkbox" id="{{amenities-id}}" value="{{ amenities-id }}" id="{{ amenities-id }}" name="amenities[]" />
+                 {{-- {{ (is_array(old('tags')) && in_array($tag->id, old('tags'))) ? ' checked' : '' }} --}}
+            <label class="form-check-label" for="{{amenities}}">{{Amenities name}}</label>
+            {{-- endforeach --}}
+        </div>
+
+        {{-- pubblicazione --}}
+        {{-- //TODO select o button? --}}
+        <div class="form-group">
+            <label for="visible">Select an option</label>
+            <select class="form-select" aria-label="Default select example" name="visible" id="visible">
+                <option selected value="">Select an option</option>
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+            </select>
+        </div>
+
+        {{-- submit button --}}
+        <button type="submit">Add places</button>
     </form>
 @endsection
--- resources/views/host/places/index.blade.php
+++ resources/views/host/places/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 {{-- button per creare nuovo place --}}
-<div class="container">
+<div class="container py-2">
   <a href="{{ route('host.places.create') }}">
     <button type="submit" class="btn btn-primary">Create new place</button>
   </a>
@@ -10,15 +10,17 @@
 
 {{-- tabella riepilogativa dei places dell'user loggato --}}
 <div class="container">
-  <table>
-    <thead>
+  <table class="table">
+    <thead class="table-dark">
       <th>Denomination</th>
       <th>Address</th>
+      {{-- //TODO aggiungere campi per modifica, elimina, statistiche --}}
     </thead>
     <tbody>
       @foreach ($places as $place)
         <td>{{ $place->title }}</td>
         <td>{{ $place->address }}</td>
+        {{-- //TODO aggiungere tasti per modifica, elimina, statistiche --}}
       @endforeach
     </tbody>
   </table>
