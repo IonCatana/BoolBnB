1f13dcfe8006b207f629ad0795455fc43003a5c2
Siripiri-hue <daemonprocellae@live.it>
2022-05-27 14:21:22 +0200

    Add index table + create button

--- resources/views/host/places/index.blade.php
+++ resources/views/host/places/index.blade.php
@@ -1,10 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>ciao sono index</div>
-    <ul>
-      @foreach ($places as $place )
-        <li>{{$place->title}}</li>
+{{-- button per creare nuovo place --}}
+<div class="container">
+  <a href="{{ route('host.places.create') }}">
+    <button type="submit" class="btn btn-primary">Create new place</button>
+  </a>
+</div>
+
+{{-- tabella riepilogativa dei places dell'user loggato --}}
+<div class="container">
+  <table>
+    <thead>
+      <th>Denomination</th>
+      <th>Address</th>
+    </thead>
+    <tbody>
+      @foreach ($places as $place)
+        <td>{{ $place->title }}</td>
+        <td>{{ $place->address }}</td>
       @endforeach
-    </ul>
+    </tbody>
+  </table>
+</div>
 @endsection
