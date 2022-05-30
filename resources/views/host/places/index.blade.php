9f43dde1151d8688c0589dd94b92fe785e95ad29
Antonio Bove <antoniofbove@gmail.com>
2022-05-30 20:56:10 +0200

    Merge branch 'develop'

--- resources/views/host/places/index.blade.php
+++ resources/views/host/places/index.blade.php
@@ -20,9 +20,8 @@
                 <th>Square Meters</th>
                 <th>Amenities</th>
                 <th>Image</th>
-                <th></th>
-                <th></th>
-                <th></th>
+                <th>Actions</th>
+                
                 {{-- //TODO aggiungere collegamento alla view front/show --}}
             </thead>
 
@@ -44,15 +43,22 @@
                         <td>
                             <img class="w-75" src="{{ asset('storage/' . $place->img)}}" alt="">
                         </td>
-                        <td><a href="{{ route('host.places.edit', $place) }}" class="btn btn-warning">Modify place</a></td>
                         <td>
+                            <a href="{{ route('host.places.edit', $place) }}" class="btn btn-warning">Edit</a>
+                        
                             <form class="form-group" action="{{ route('host.places.destroy', $place) }}"
                                 method="POST">
                                 @csrf
                                 @method('DELETE')
 
-                                <button type="submit" class="btn btn-danger">Elimina post</button>
+                                <button type="submit" class="btn btn-danger">Delete</button>
                             </form>
+
+                            @if (!$place->visible)
+                                <a href="{{ route('host.places.toggleVisibility', $place) }}" class="btn btn-dark">Visibility:Off</a>
+                            @else
+                                <a href="{{ route('host.places.toggleVisibility', $place) }}" class="btn btn-success">Visibility:On</a>
+                            @endif
                         </td>
                         {{-- //TODO aggiungere tasto/link per front/show-- --}}
                 @endforeach
