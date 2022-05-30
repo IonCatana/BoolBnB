fee0aaa4c5ff6c6a6f576eeda594c25738bf254c
Antonio Bove <antoniofbove@gmail.com>
2022-05-30 16:07:09 +0200

    init vue &vue-router

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
@@ -40,6 +40,6 @@ Route::prefix('host')
         ]);
     });
 
-Route::options('/{path}', function(){ 
-    return '';
-})->where('path', '.*');
+Route::get('/{any?}', function(){ 
+    return view('guest.app');
+})->where('any', '.*');
