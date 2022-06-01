dcc4ebcaf64f64c50c7c9bd000990653a0e70063
Antonio Bove <antoniofbove@gmail.com>
2022-06-01 12:46:45 +0200

    middleware ok

--- app/Http/Middleware/CheckVisibilityPermissions.php
+++ app/Http/Middleware/CheckVisibilityPermissions.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use App\Place;
 use Closure;
+use Illuminate\Support\Str;
 
 class CheckVisibilityPermissions
 {
@@ -25,13 +26,13 @@ class CheckVisibilityPermissions
                 $missing_attributes[] = 'amenities';
             }
 
-            //check img presence
-            Place::where()
+            //controllo se img è nella request o è gia in db
+            $place = Place::where('slug', Str::slug($attributes['title']))->first();
+            if (!array_key_exists('img', $attributes)
+                && ($place != null && blank($place->img) || $place == null)
+                ) $missing_attributes[] = 'img';
 
-            if (!array_key_exists('img', $attributes)) {
-               $missing_attributes[] = 'img';
-            }
-
+            // controllo se manca qualche altro attributo
             foreach ($attributes as $attribute => $value) {
                 if (blank($value)) {
                     $missing_attributes[] = $attribute;
@@ -39,9 +40,11 @@ class CheckVisibilityPermissions
             }
             dd($missing_attributes);
 
-            if (!empty($missing_attributes)) {
-                return redirect()->route('host.places.fillAttributes', compact('missing_attributes'));
+            if (empty($missing_attributes)) {
+                return $next($request);
             }
+
+            return redirect()->route('host.places.fillAttributes', compact('missing_attributes'));
         }
 
         return $next($request);
