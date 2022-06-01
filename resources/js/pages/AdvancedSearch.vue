c39096a25668e65058097e187270f1c99b4dc880
IonCatana <catana.ion17@yahoo.it>
2022-06-01 10:29:04 +0200

    bug

---
+++ resources/js/pages/AdvancedSearch.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="advanced_search">
+    <h1>vue di merda</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: AdvancedSearch,
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
