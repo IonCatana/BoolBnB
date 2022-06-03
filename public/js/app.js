e36b742e569aba772f62f400b3e92b2fbcede1c5
Siripiri-hue <daemonprocellae@live.it>
2022-06-03 16:07:06 +0200

    added comments

--- public/js/app.js
+++ public/js/app.js
@@ -37535,11 +37535,11 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 // const { isEmpty } = require("lodash");
 
 var validationErrors = []; //array che contiene gli errori
-// Compare alert se non è stata checkata almeno una checkbox delle amenities
+//prende i pulsanti submit dei form create e edit
 
-var submitButtons = document.getElementById('form-submit-button');
-var checkboxes = document.getElementsByClassName('validation-amenity'); // let arrayChecked = [];
-//fn che controlla se almeno una checkbox è checkata
+var submitButtons = document.getElementById('form-submit-button'); //prende le checkbox delle amenities
+
+var checkboxes = document.getElementsByClassName('validation-amenity'); //fn che controlla se almeno una checkbox è checkata
 
 function checkAmenities() {
   var _iterator = _createForOfIteratorHelper(checkboxes),
@@ -37561,11 +37561,27 @@ function checkAmenities() {
 
   validationErrors.push('- Select at least one amenity');
   return false;
-}
+} // se c'è qualche errore, compare alert, quando si invia il form
+
 
 submitButtons.addEventListener('click', function () {
-  validationErrors = [];
-  checkAmenities();
+  // resetto l'array nel caso venga ricaricata la pagina
+  validationErrors = []; // richiamo fn che controlla le checkboxes
+
+  checkAmenities(); //controllo dei campi
+  // let title = document.forms["place-form"]["title"].value;
+  // if (title == "") 
+  //     alert("Title must be filled out");
+  // let address = document.forms["place-form"]["address"].value;
+  // if (address == "") 
+  //     alert("Address must be filled out");
+  // let lat = document.forms["place-form"]["lat"].value;
+  // if (lat == "") 
+  //     alert("Latitude must be filled out");
+  // let lon = document.forms["place-form"]["lon"].value;
+  // if (lon == "") 
+  //     alert("Longitude must be filled out");
+
   var rooms = document.forms["place-form"]["rooms"].value;
 
   if (!Object(lodash__WEBPACK_IMPORTED_MODULE_0__["isEmpty"])(rooms)) {
@@ -37601,54 +37617,7 @@ submitButtons.addEventListener('click', function () {
   }
 
   if (validationErrors.length > 0) alert(validationErrors.join("\n"));
-}); //prendo i pulsanti submit dei form create e edit
-// const submitButtons = document.getElementById('form-submit-button');
-//quando si preme submit eseguo la fn di verifica
-// submitButtons.addEventListener('click', function() {
-//     // let title = document.forms["place-form"]["title"].value;
-//     // if (title == "") 
-//     //     alert("Title must be filled out");
-//     // let address = document.forms["place-form"]["address"].value;
-//     // if (address == "") 
-//     //     alert("Address must be filled out");
-//     // let lat = document.forms["place-form"]["lat"].value;
-//     // if (lat == "") 
-//     //     alert("Latitude must be filled out");
-//     // let lon = document.forms["place-form"]["lon"].value;
-//     // if (lon == "") 
-//     //     alert("Longitude must be filled out");
-//     let rooms = document.forms["place-form"]["rooms"].value;
-//     if ( !(isEmpty(rooms) ) ) {
-//         if ( isNaN(rooms) || rooms < 1 )
-//             validationErrors.push("- Rooms must be a number and greater than 0");
-//     }
-//     let beds = document.forms["place-form"]["beds"].value;
-//     if ( !(isEmpty(beds) ) ) {
-//         if (isNaN(beds) || beds < 1) 
-//             validationErrors.push("- Beds must be a number and greater than 0");
-//     }
-//     let baths = document.forms["place-form"]["bathrooms"].value;
-//     if ( !(isEmpty(baths) ) ) {
-//         if (isNaN(baths) || baths < 1) 
-//             validationErrors.push("- Bathrooms must be a number and greater than 0");
-//     }
-//     let squareM = document.forms["place-form"]["square_meters"].value;
-//     if ( !(isEmpty(squareM) ) ) {
-//         if (isNaN(squareM) || squareM < 1)
-//             validationErrors.push("- Square meters must be a number and greater than 0");
-//     }
-//     let imgInput = document.forms["place-form"]["img"];
-//     let imgPath = imgInput.value;
-//     let allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
-//     if ( !(isEmpty(imgPath) ) ) {
-//         if (!allowedExtensions.exec(imgPath)) {
-//             validationErrors.push('- Invalid file type');
-//         }
-//     }
-//     if (validationErrors.length > 0)
-//         alert(validationErrors.join("\n"));
-// });
-//TODO ?
+}); //TODO ?
 //visibility
 //prendo il pulsante submit del form visibility
 // const visibility = document.getElementById('visibility-btn');
--- resources/js/host/validation.js
+++ resources/js/host/validation.js
@@ -4,10 +4,10 @@ import { isEmpty } from 'lodash';
 
 let validationErrors = []; //array che contiene gli errori
 
-// Compare alert se non è stata checkata almeno una checkbox delle amenities
+//prende i pulsanti submit dei form create e edit
 const submitButtons = document.getElementById('form-submit-button');
+//prende le checkbox delle amenities
 const checkboxes = document.getElementsByClassName('validation-amenity');
-// let arrayChecked = [];
 
 //fn che controlla se almeno una checkbox è checkata
 function checkAmenities () {
@@ -22,11 +22,32 @@ function checkAmenities () {
     return false;
 }
 
+// se c'è qualche errore, compare alert, quando si invia il form
 submitButtons.addEventListener('click', function() {
 
+    // resetto l'array nel caso venga ricaricata la pagina
     validationErrors = [];
     
+    // richiamo fn che controlla le checkboxes
     checkAmenities();
+
+    //controllo dei campi
+    
+    // let title = document.forms["place-form"]["title"].value;
+    // if (title == "") 
+    //     alert("Title must be filled out");
+
+    // let address = document.forms["place-form"]["address"].value;
+    // if (address == "") 
+    //     alert("Address must be filled out");
+
+    // let lat = document.forms["place-form"]["lat"].value;
+    // if (lat == "") 
+    //     alert("Latitude must be filled out");
+
+    // let lon = document.forms["place-form"]["lon"].value;
+    // if (lon == "") 
+    //     alert("Longitude must be filled out");
 
     let rooms = document.forms["place-form"]["rooms"].value;
     if ( !(isEmpty(rooms) ) ) {
@@ -65,65 +86,6 @@ submitButtons.addEventListener('click', function() {
     if (validationErrors.length > 0)
         alert(validationErrors.join("\n"));
 });
-
-//prendo i pulsanti submit dei form create e edit
-// const submitButtons = document.getElementById('form-submit-button');
-
-//quando si preme submit eseguo la fn di verifica
-// submitButtons.addEventListener('click', function() {
-//     // let title = document.forms["place-form"]["title"].value;
-//     // if (title == "") 
-//     //     alert("Title must be filled out");
-
-//     // let address = document.forms["place-form"]["address"].value;
-//     // if (address == "") 
-//     //     alert("Address must be filled out");
-
-//     // let lat = document.forms["place-form"]["lat"].value;
-//     // if (lat == "") 
-//     //     alert("Latitude must be filled out");
-
-//     // let lon = document.forms["place-form"]["lon"].value;
-//     // if (lon == "") 
-//     //     alert("Longitude must be filled out");
-
-//     let rooms = document.forms["place-form"]["rooms"].value;
-//     if ( !(isEmpty(rooms) ) ) {
-//         if ( isNaN(rooms) || rooms < 1 )
-//             validationErrors.push("- Rooms must be a number and greater than 0");
-//     }
-
-//     let beds = document.forms["place-form"]["beds"].value;
-//     if ( !(isEmpty(beds) ) ) {
-//         if (isNaN(beds) || beds < 1) 
-//             validationErrors.push("- Beds must be a number and greater than 0");
-//     }
-
-//     let baths = document.forms["place-form"]["bathrooms"].value;
-//     if ( !(isEmpty(baths) ) ) {
-//         if (isNaN(baths) || baths < 1) 
-//             validationErrors.push("- Bathrooms must be a number and greater than 0");
-//     }
-
-//     let squareM = document.forms["place-form"]["square_meters"].value;
-//     if ( !(isEmpty(squareM) ) ) {
-//         if (isNaN(squareM) || squareM < 1)
-//             validationErrors.push("- Square meters must be a number and greater than 0");
-//     }
-
-//     let imgInput = document.forms["place-form"]["img"];
-//     let imgPath = imgInput.value;
-//     let allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
-
-//     if ( !(isEmpty(imgPath) ) ) {
-//         if (!allowedExtensions.exec(imgPath)) {
-//             validationErrors.push('- Invalid file type');
-//         }
-//     }
-
-//     if (validationErrors.length > 0)
-//         alert(validationErrors.join("\n"));
-// });
 
 //TODO ?
 //visibility
